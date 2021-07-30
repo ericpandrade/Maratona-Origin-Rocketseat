d4e6ce7a366bf467c906a6c7de6f3976207a2ccb
Eric Pereira Andrade <ericpandrade085@gmail.com>
2021-07-29 23:44:48 -0300

    final

--- main.js
+++ main.js
@@ -19,8 +19,9 @@ for(const link of links) {
 
 /* Box shadow no scroll da página */
 
+const header = document.querySelector("#header")
+
 function changeHeaderWhenScroll () {
-    const header = document.querySelector("#header")
     const navHeight = header.offsetHeight
 
     if (window.scrollY >= navHeight) {
@@ -63,13 +64,37 @@ scrollReveal.reveal(
     
 /* Botão voltar para o topo */
 
-function backToTop() {
-    const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector('.back-to-top')
 
+function backToTop() {
     if (window.scrollY >= 560) {
         backToTopButton.classList.add('show')
     } else {
         backToTopButton.classList.remove('show')
+    }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuCurrentSection () {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        } else {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
     }
 }
 
@@ -78,5 +103,5 @@ function backToTop() {
 window.addEventListener('scroll', function() {
     changeHeaderWhenScroll()
     backToTop()
+    activateMenuCurrentSection ()
 })
-
